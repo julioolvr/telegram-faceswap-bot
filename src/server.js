4f16538384261d63299c18a8d6d89e69d07476c7
Julio Olivera <julio.olvr@gmail.com>
2015-08-07 20:39:27 -0300

    List of allowed ids

--- src/server.js
+++ src/server.js
@@ -4,14 +4,24 @@ import TelegramClient from './services/telegramClient';
 const bot = new Bot();
 const telegramClient = new TelegramClient(process.env.BOT_TOKEN);
 
+const allowedChatIds = process.env.ALLOWED_CHAT_IDS.split(';');
+
 class App {
   start() {
     this.waitForNextResponse();
   }
 
   waitForNextResponse() {
+    console.log('allowed ids are', allowedChatIds);
     telegramClient.getUpdates().then(messages => {
-      messages.forEach(bot.respondTo.bind(bot));
+      messages.forEach(message => {
+        if (allowedChatIds.indexOf(message.chat.id.toString()) === -1) {
+          console.log('Unkown message from chat id', message.chat.id, message);
+          return;
+        }
+
+        bot.respondTo(message);
+      });
       this.waitForNextResponse();
     });
   }
