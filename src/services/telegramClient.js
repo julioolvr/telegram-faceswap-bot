3fedbb31aa7a34adedcff2738d6c4ac59fee801d
Julio Olivera <julio.olvr@gmail.com>
2015-08-08 03:23:43 -0300

    telegramClient tests

--- src/services/telegramClient.js
+++ src/services/telegramClient.js
@@ -1,46 +1,65 @@
 import request from 'request-promise';
 
 export default class {
-    constructor(token) {
-      this.baseUrl = `https://api.telegram.org/bot${token}`;
+  /**
+   * Builds a client for the given bot token.
+   * @param {string} token Token for the bot provided by Telegram.
+   */
+  constructor(token) {
+    this.baseUrl = `https://api.telegram.org/bot${token}`;
+  }
+
+  /**
+   * Sends a text message to the given chatId.
+   * @param  {string} text   Text to send on the message.
+   * @param  {string} chatId Id of the chat to send the message to.
+   * @return {Promise}       A promise for the request to Telegram's API.
+   */
+  sendText(text, chatId) {
+    return request.post(`${this.baseUrl}/sendMessage`, { form: { text: text, chat_id: chatId } });
+  }
+
+  /**
+   * Sends a photo to the given chatId.
+   * @param  {Promise} image  A promise that resolves with a buffer for the photo.
+   * @param  {string}  chatId Id of the chat to send the message to.
+   * @return {Promise}        A promise for the request to Telegram's API.
+   */
+  sendPhoto(image, chatId) {
+    return image.then(buffer => {
+      return request.post(`${this.baseUrl}/sendPhoto`, { formData: {
+                                                          photo: {
+                                                            value: buffer,
+                                                            options: {
+                                                              contentType: 'image/png',
+                                                              filename: 'faceSwap.png'
+                                                            }
+                                                          }, chat_id: chatId } });
+    });
+  }
+
+  /**
+   * Long-polls Telegram's API to get new messages. It will remember the latest update and
+   * query starting from the next one on subsequent calls.
+   * @return {Promise} A promise for the request to Telegram's API, converting updates to messages.
+   */
+  getUpdates() {
+    let options = { timeout: 60 };
+
+    if (this.lastOffset !== undefined) {
+      options.offset = this.lastOffset + 1;
     }
 
-    sendText(text, chatId) {
-      return request.post(`${this.baseUrl}/sendMessage`, { form: { text: text, chat_id: chatId } });
-    }
+    return request.get({url: `${this.baseUrl}/getUpdates`, qs: options})
+      .then(response => JSON.parse(response).result)
+      .then(updates => {
+        if (updates.length === 0) {
+          return [];
+        }
 
-    sendPhoto(image, chatId) {
-      return image.then(function(buffer) {
-        return request.post(`${this.baseUrl}/sendPhoto`, { formData: {
-                                                            photo: {
-                                                              value: buffer,
-                                                              options: {
-                                                                contentType: 'image/png',
-                                                                filename: 'faceSwap.png'
-                                                              }
-                                                            }, chat_id: chatId } });
-      }.bind(this));
-    }
-
-    getUpdates() {
-      let options = { timeout: 60 };
-
-      if (this.lastOffset !== undefined) {
-        options.offset = this.lastOffset + 1;
-      }
-
-      return request.get({url: `${this.baseUrl}/getUpdates`, qs: options})
-        .then(response => JSON.parse(response).result)
-        .then(updates => {
-          if (updates.length === 0) {
-            return [];
-          }
-
-          let ids = updates.map(update => update.update_id);
-          console.log(ids);
-          this.lastOffset = Math.max(...ids);
-          console.log('max id', this.lastOffset);
-          return updates.map(update => update.message);
-        });
-    }
+        let ids = updates.map(update => update.update_id);
+        this.lastOffset = Math.max(...ids);
+        return updates.map(update => update.message);
+      });
+  }
 }
