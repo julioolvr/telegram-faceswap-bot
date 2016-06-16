509867e6d13ceb67f2eb0ea88727973b2b5807dd
Julio Olivera <julio.olvr@gmail.com>
2016-06-16 10:54:44 -0300

    Fix custom image search

--- src/services/googleImages.js
+++ src/services/googleImages.js
@@ -7,12 +7,10 @@ function randomSearchKey() {
 
 export const search = (query) => {
   const url =
-    `https://www.googleapis.com/customsearch/v1?key=${randomSearchKey()}&cx=${process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID}&q=${encodeURIComponent(query)}`
+    `https://www.googleapis.com/customsearch/v1?key=${randomSearchKey()}&cx=${process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID}&q=${encodeURIComponent(query)}&searchType=image`
 
   return got(url, { json: true })
     .then(response => response.body)
     .then(data => data.items ? data.items : [])
-    .then(items => items.map(item => item.pagemap.imageobject[0]))
-    .then(imageObjects => imageObjects.map(imageObject => imageObject.url || imageObject.contenturl))
-    .then(imagesUrls => imagesUrls.filter(url => url))
+    .then(items => items.map(item => item.link))
 }
